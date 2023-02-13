cf84888c95e21b59856f6e25234b16b4c032f391
Jishnu <jishnupvkd@gmail.com>
2023-02-13 23:48:38 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UniLinks Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UniLinks Demo'),
+      ),
+      body: const Center(
+        child: Text('my home page')
+      ),
+    );
+  }
+}
